1f75483a53decc6f1cfa9d79d33c4b261d108555
Mark Norman Francis <norm@201created.com>
2019-09-02 14:59:54 +0100

    Test basic expansion works

--- sectile/__init__.py
+++ sectile/__init__.py
@@ -1,3 +1,37 @@
+import re
+
+
+SECTILE_COMMAND = r"""
+    ^
+
+    # grab what comes before the sectile command
+    (?P<before> .* )
+
+    # the sectile command, grab the name of the inserted file
+    \<\% \s* sectile \s+ insert \s+ (?P<insert> .*? ) \s* \%\>
+
+    # grab what comes after the sectile command
+    (?P<after> .* )
+
+    $
+"""
+
+INSERT = 'INSERTED TEXT'
+
+
 class Sectile(object):
+    def __init__(self):
+        self.matcher = re.compile(
+            SECTILE_COMMAND,
+            re.MULTILINE|re.DOTALL|re.VERBOSE
+        )
+
     def expand(self, string):
-        return string
+        matches = re.search(self.matcher, string)
+        expanded = string
+
+        if matches is not None:
+            if matches.group('insert'):
+                expanded = matches.group('before') + INSERT + matches.group('after')
+
+        return expanded
