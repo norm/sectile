8c502c1c4e337a692aae11f0276ce8ab6ef39392
Mark Norman Francis <norm@201created.com>
2019-09-02 15:19:49 +0100

    Read insertions from files

--- tests/test_expand.py
+++ tests/test_expand.py
@@ -5,21 +5,27 @@ from sectile import Sectile
 
 def test_expand_raises_error_without_arguments():
     with pytest.raises(TypeError):
-        sectile = Sectile()
+        sectile = Sectile('tests/fragments')
         sectile.expand()
+
 
 def test_expand_raises_error_on_nonstring():
     with pytest.raises(TypeError):
-        sectile = Sectile()
+        sectile = Sectile('tests/fragments')
         sectile.expand(1234)
 
+
 def test_expand_returns_strings_unchanged():
-    sectile = Sectile()
+    sectile = Sectile('tests/fragments')
     assert sectile.expand('') == ''
     assert sectile.expand('a string') == 'a string'
     assert sectile.expand('\nhello\n') == '\nhello\n'
     assert sectile.expand('1234') == '1234'
 
+
 def test_expand_expands():
-    sectile = Sectile()
-    assert sectile.expand('\n<% sectile insert me %>\n') == '\nINSERTED TEXT\n'
+    sectile = Sectile('tests/fragments')
+    assert (
+        sectile.expand('\n<% sectile insert something %>\n')
+        == '\nFOUND SOMETHING\n'
+    )
